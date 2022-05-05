4316bbff1849597e48f277f76090a739a366bb66
Geliel Santos <Geliel Santos>
2022-05-05 17:28:32 -0300

    API funcionando

--- geliel_santos/src/App.tsx
+++ geliel_santos/src/App.tsx
@@ -1,5 +1,37 @@
-function App() {
-  return <h1>Hello World</h1>
+import React, { Component } from "react";
+import { render } from "react-dom";
+import api from './Api';
+
+class App extends Component{
+
+  state = {artigos: [],}
+
+  async componentDidMount(){
+
+    const response = await api.get('');
+    //console.log(response.data)
+
+    this.setState({artigos: response.data})
+  }
+
+  render(){
+    const { artigos } = this.state;
+    return(
+      <div>
+        <h1>Listar os artigos: </h1>
+        {console.log(artigos)}
+        {artigos.map(artigo =>(
+          <li key = {artigo.id}>
+            <h2>
+              <strong>Título: </strong>
+              {artigo.title}
+            </h2>
+          </li>
+        ))}
+      </div>
+    )
+  }
 }
 
-export default App
+
+export default App;
